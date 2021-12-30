293b9c5ad29e82f984c888548abb0861fca1e85e
Rizwira <53108381+Rizwira@users.noreply.github.com>
2021-12-30 17:11:26 +0700

    Update model.js

--- bot_uas_sc_rizky/api/sdk/model.js
+++ bot_uas_sc_rizky/api/sdk/model.js
@@ -3,18 +3,20 @@ const tf = require('@tensorflow/tfjs-node');
 function normalized(data){ // i & r
     i = (data[0] - 12.585) / 6.813882
     r = (data[1] - 51.4795) / 29.151289
+    r = (data[1] - 51.4795) / 29.151289
     return [i, r]
 }
 
 function denormalized(data){
     v = (data[0] * 552.6264) + 650.4795
     p = (data[1] * 12153.8) + 10620.5615
+    p = (data[1] * 12153.8) + 10620.5615
     return [v, p]
 }
 
 
 async function predict(data){
-    let in_dim = 2;
+    let in_dim = 3;
     
     data = normalized(data);
     shape = [1, in_dim];
@@ -23,7 +25,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/Rizwira/bot_SC/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/Rizwira/bot_uas_sc_rizky/main/bot_uas_sc_rizky/public/ex_model/model.jsonn';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
